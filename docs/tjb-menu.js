72e2bdc9ba7aa6278a62881b597ffb5a23fef52e
Thibault Jan Beyer <hi@thibaultjanbeyer.com>
2019-01-11 23:28:49 +0100

    fix webcomponent extending issue

--- docs/tjb-menu.js
+++ docs/tjb-menu.js
@@ -1,169 +1,171 @@
-import WebComponent from 'https://tjb-webcomponents.github.io/tjb-webcomponent/tjb-wc.min.js';
-import html from 'https://tjb-webcomponents.github.io/html-template-string/html.min.js';
+import WebComponent from "https://tjb-webcomponents.github.io/tjb-webcomponent/tjb-wc.min.js";
+import html from "https://tjb-webcomponents.github.io/html-template-string/html.min.js";
 
 class tjbMenu extends WebComponent() {
   CSS() {
-    return html`<style>
-      :host {
-        --tjb-menu-sub-bg: white;
-        --tjb-menu-sub-b: 1px solid rgba(100, 100, 100, 0.5);
-        --tjb-menu-sub-c: black;
-        --tjb-menu-toggler-hover: blue;
-        --tjb-menu-toggler-color: white;
-        --tjb-menu-mobile-bg: lightgrey;
-        --tjb-menu-mobile-c: inherit;
-      }
+    return html`
+      <style>
+        :host {
+          --tjb-menu-sub-bg: white;
+          --tjb-menu-sub-b: 1px solid rgba(100, 100, 100, 0.5);
+          --tjb-menu-sub-c: black;
+          --tjb-menu-toggler-hover: blue;
+          --tjb-menu-toggler-color: white;
+          --tjb-menu-mobile-bg: lightgrey;
+          --tjb-menu-mobile-c: inherit;
+        }
 
-      .menu {
-        align-items: center;
-        display: grid;
-        grid-template-columns: auto auto;
-        justify-content: space-between;
-        position: relative;
-      }
+        .menu {
+          align-items: center;
+          display: grid;
+          grid-template-columns: auto auto;
+          justify-content: space-between;
+          position: relative;
+        }
 
-      .visuallyhidden {
-        border: 0;
-        clip: rect(0, 0, 0, 0);
-        margin: -1px;
-        padding: 0;
-        position: absolute;
-        height: 1px;
-        overflow: hidden;
-        width: 1px;
-      }
+        .visuallyhidden {
+          border: 0;
+          clip: rect(0, 0, 0, 0);
+          margin: -1px;
+          padding: 0;
+          position: absolute;
+          height: 1px;
+          overflow: hidden;
+          width: 1px;
+        }
 
-      /* Functionality */
-      .menu__checkbox {
-        opacity: 0;
-      }
-
-      .menu__checkbox:checked ~ .menu__underlay,
-      .menu__checkbox:checked ~ .menu__container {
-        transform: translateX(0);
-      }
-
-      .menu__toggle:hover,
-      .menu__toggle:active,
-      .menu__checkbox:focus ~ .menu__toggle {
-        border-color: var(--tjb-menu-toggler-hover);
-      }
-      .menu__toggle:hover::before,
-      .menu__toggle:hover::after,
-      .menu__toggle:active::before,
-      .menu__toggle:active::after,
-      .menu__checkbox:focus ~ .menu__toggle::before,
-      .menu__checkbox:focus ~ .menu__toggle::after {
-        background-color: var(--tjb-menu-toggler-hover);
-      }
-
-      .menu__checkbox:checked ~ .menu__toggle {
-        border-width: 0;
-      }
-      .menu__checkbox:checked ~ .menu__toggle::before {
-        transform: rotate(45deg);
-      }
-      .menu__checkbox:checked ~ .menu__toggle::after {
-        transform: rotate(-45deg);
-      }
-
-      .menu__toggle {
-        border-bottom: 3px solid var(--tjb-menu-toggler-color);
-        border-top: 3px solid var(--tjb-menu-toggler-color);
-        box-sizing: border-box;
-        cursor: pointer;
-        height: 20px;
-        position: absolute;
-        right: 0;
-        top: -2px;
-        transition: border-width 250ms ease-in-out;
-        width: 25px;
-        z-index: 1001;
-      }
-
-      .menu__toggle::after,
-      .menu__toggle::before {
-        background-color: var(--tjb-menu-toggler-color);
-        content: "";
-        height: 3px;
-        left: 0;
-        position: absolute;
-        top: calc(50% - 1.5px);
-        transition: transform 250ms ease-in-out;
-        width: 100%;
-      }
-
-      .menu__container {
-        background-color: var(--tjb-menu-mobile-bg);
-        color: var(--tjb-menu-mobile-c);
-        height: 100vh;
-        position: fixed;
-        transform: translateX(111%);
-        transition: transform 250ms ease-in-out;
-        width: 250px;
-        text-align: center;
-        z-index: 1000;
-        right: -5px;
-        top: 0;
-      }
-
-      .menu__underlay {
-        backdrop-filter: blur(10px);
-        position: fixed;
-        top: 0;
-        left: 0;
-        background: #000;
-        opacity: 0.7;
-        width: 100%;
-        height: 100%;
-        transform: translateX(100%);
-        transition: transform 300ms ease-in-out;
-      }
-
-      .menu__ul {
-        list-style: none;
-        margin: 20px;
-        padding: 0;
-      }
-
-      @media screen and (min-width: 600px) {
+        /* Functionality */
         .menu__checkbox {
-          display: none;
+          opacity: 0;
+        }
+
+        .menu__checkbox:checked ~ .menu__underlay,
+        .menu__checkbox:checked ~ .menu__container {
+          transform: translateX(0);
+        }
+
+        .menu__toggle:hover,
+        .menu__toggle:active,
+        .menu__checkbox:focus ~ .menu__toggle {
+          border-color: var(--tjb-menu-toggler-hover);
+        }
+        .menu__toggle:hover::before,
+        .menu__toggle:hover::after,
+        .menu__toggle:active::before,
+        .menu__toggle:active::after,
+        .menu__checkbox:focus ~ .menu__toggle::before,
+        .menu__checkbox:focus ~ .menu__toggle::after {
+          background-color: var(--tjb-menu-toggler-hover);
+        }
+
+        .menu__checkbox:checked ~ .menu__toggle {
+          border-width: 0;
+        }
+        .menu__checkbox:checked ~ .menu__toggle::before {
+          transform: rotate(45deg);
+        }
+        .menu__checkbox:checked ~ .menu__toggle::after {
+          transform: rotate(-45deg);
         }
 
         .menu__toggle {
-          display: none;
+          border-bottom: 3px solid var(--tjb-menu-toggler-color);
+          border-top: 3px solid var(--tjb-menu-toggler-color);
+          box-sizing: border-box;
+          cursor: pointer;
+          height: 20px;
+          position: absolute;
+          right: 0;
+          top: -2px;
+          transition: border-width 250ms ease-in-out;
+          width: 25px;
+          z-index: 1001;
+        }
+
+        .menu__toggle::after,
+        .menu__toggle::before {
+          background-color: var(--tjb-menu-toggler-color);
+          content: "";
+          height: 3px;
+          left: 0;
+          position: absolute;
+          top: calc(50% - 1.5px);
+          transition: transform 250ms ease-in-out;
+          width: 100%;
         }
 
         .menu__container {
-          background-color: inherit;
-          border: 0;
-          height: auto;
-          position: inherit;
-          transform: translateX(0);
-          width: auto;
+          background-color: var(--tjb-menu-mobile-bg);
+          color: var(--tjb-menu-mobile-c);
+          height: 100vh;
+          position: fixed;
+          transform: translateX(111%);
+          transition: transform 250ms ease-in-out;
+          width: 250px;
+          text-align: center;
+          z-index: 1000;
+          right: -5px;
+          top: 0;
         }
 
         .menu__underlay {
-          display: none;
+          backdrop-filter: blur(10px);
+          position: fixed;
+          top: 0;
+          left: 0;
+          background: #000;
+          opacity: 0.7;
+          width: 100%;
+          height: 100%;
+          transform: translateX(100%);
+          transition: transform 300ms ease-in-out;
         }
 
         .menu__ul {
-          margin: 0;
-          text-align: right;
+          list-style: none;
+          margin: 20px;
+          padding: 0;
         }
-      }
-    </style>`;
+
+        @media screen and (min-width: 600px) {
+          .menu__checkbox {
+            display: none;
+          }
+
+          .menu__toggle {
+            display: none;
+          }
+
+          .menu__container {
+            background-color: inherit;
+            border: 0;
+            height: auto;
+            position: inherit;
+            transform: translateX(0);
+            width: auto;
+          }
+
+          .menu__underlay {
+            display: none;
+          }
+
+          .menu__ul {
+            margin: 0;
+            text-align: right;
+          }
+        }
+      </style>
+    `;
   }
 
   HTML() {
     this.checkboxNode = html`
-    <input
-      type="checkbox"
-      id="menu-toggler"
-      aria-hidden="true"
-      class="menu__checkbox visuallyhidden"
-    />
+      <input
+        type="checkbox"
+        id="menu-toggler"
+        aria-hidden="true"
+        class="menu__checkbox visuallyhidden"
+      />
     `;
     return html`
       <div class="menu">
@@ -180,7 +182,10 @@ class tjbMenu extends WebComponent() {
             for="menu-toggler"
           ></label>
 
-          <div class="menu__underlay" onclick="${e => this.handleUnderlayClick()}"></div>
+          <div
+            class="menu__underlay"
+            onclick="${e => this.handleUnderlayClick()}"
+          ></div>
           <div class="menu__container">
             <ul class="menu__ul" id="menu-items">
               <slot name="menu-item"></slot>
@@ -196,10 +201,10 @@ class tjbMenu extends WebComponent() {
   }
 }
 
-class tjbMenuItemSub extends WebComponent {
+class tjbMenuItemSub extends WebComponent() {
   CSS() {
     return html`
-    <style>
+      <style>
         .visuallyhidden {
           border: 0;
           clip: rect(0, 0, 0, 0);
@@ -262,7 +267,7 @@ class tjbMenuItemSub extends WebComponent {
           --modifier-padding: 5px 20px 10px;
         }
       </style>
-      `;
+    `;
   }
 
   HTML() {
@@ -276,15 +281,11 @@ class tjbMenuItemSub extends WebComponent {
     `;
 
     this.selectorNode = html`
-      <slot
-        name="menu-item"
-        class="menu__sub-li"
-      ></slot>
+      <slot name="menu-item" class="menu__sub-li"></slot>
     `;
 
     return html`
       <li class="menu__li">
-
         ${this.checkboxNode}
 
         <label for="menu__sub-toggle" class="menu__sub-toggle">
@@ -320,7 +321,7 @@ class tjbMenuItemSub extends WebComponent {
   }
 }
 
-class tjbMenuItem extends WebComponent {
+class tjbMenuItem extends WebComponent() {
   CSS() {
     return html`
       <style>
@@ -349,9 +350,7 @@ class tjbMenuItem extends WebComponent {
 
   HTML() {
     return html`
-      <li class="menu__li">
-        <slot name="menu-item-link"></slot>
-      </li>
+      <li class="menu__li"><slot name="menu-item-link"></slot></li>
     `;
   }
 }
